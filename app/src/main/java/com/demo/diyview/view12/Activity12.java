e64ea79e67f78ecee32799588ad5369d98b488a3
VinVon <642751580@qq.com>
2017-08-02 15:53:22 +0800

    事件分发，侧滑菜单

--- app/src/main/java/com/demo/diyview/view12/Activity12.java
+++ app/src/main/java/com/demo/diyview/view12/Activity12.java
@@ -3,18 +3,47 @@ package com.demo.diyview.view12;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.demo.diyview.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by raytine on 2017/8/1.
  */
 
 public class Activity12 extends AppCompatActivity {
+    @BindView(R.id.left)
+    ImageView left;
+    @BindView(R.id.headView)
+    ImageView headView;
+    @BindView(R.id.sliding)
+    XjSlidingMenu sliding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity12_main);
-        getWindowManager();
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    private void initView() {
+        headView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Activity12.this, "点击头像", Toast.LENGTH_SHORT).show();
+            }
+        });
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sliding.openMenu();
+            }
+        });
     }
 }
--- app/src/main/java/com/demo/diyview/view12/XjSlidingMenu.java
+++ app/src/main/java/com/demo/diyview/view12/XjSlidingMenu.java
@@ -1,10 +1,14 @@
 package com.demo.diyview.view12;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -15,8 +19,13 @@ import android.widget.HorizontalScrollView;
  */
 
 public class XjSlidingMenu extends HorizontalScrollView {
+    private View menuView,contentView;
+    private int current;//滑动的距离
     private int distance = 62;
     private int menuWidth;
+    private GestureDetector gestureDetector;
+    private boolean menuState = false;
+    private boolean isIntercepter = false;
     private int dip2px(int i) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,i,getResources().getDisplayMetrics());
     }
@@ -34,6 +43,7 @@ public class XjSlidingMenu extends HorizontalScrollView {
         super(context, attrs, defStyleAttr);
         distance = dip2px(62);
         menuWidth = getScreenWidth()-distance;
+         gestureDetector = new GestureDetector(getContext(),simpleOnGestureListener);
     }
 
     /**
@@ -49,14 +59,14 @@ public class XjSlidingMenu extends HorizontalScrollView {
         /**
          * 菜单页
          */
-        View menuView = childAt.getChildAt(0);
+         menuView = childAt.getChildAt(0);
         ViewGroup.LayoutParams layoutParams = menuView.getLayoutParams();
         layoutParams.width = menuWidth;
-        menuView.setLayoutParams(layoutParams);
+            menuView.setLayoutParams(layoutParams);
         /**
          * 内容页
          */
-        View contentView = childAt.getChildAt(1);
+         contentView = childAt.getChildAt(1);
         ViewGroup.LayoutParams layoutParam = contentView.getLayoutParams();
         layoutParam.width = getScreenWidth();
         contentView.setLayoutParams(layoutParam);
@@ -69,6 +79,78 @@ public class XjSlidingMenu extends HorizontalScrollView {
         scrollTo(menuWidth,0);
     }
 
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        isIntercepter = false;
+            if (menuState){
+                float cyrrentX = ev.getX();
+             if (cyrrentX >menuWidth){
+                 Log.e("TAG","dispatchTouchEvent关闭菜单");
+                 closeMenu();
+                 isIntercepter = true;
+                 return  true;
+             }
+            }
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        Log.e("TAG","onTouchEvent");
+        if (isIntercepter){
+            return true;
+        }
+        boolean b = this.gestureDetector.onTouchEvent(ev);
+        if (b){
+            return b;
+        }
+        if (ev.getAction() == MotionEvent.ACTION_UP){
+                 current = getScrollX();
+            if (current > menuWidth/2){
+                //关闭菜单
+                closeMenu();
+            }else{
+                //打开菜单
+                openMenu();
+            }
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        float scale = 1f*l/menuWidth;
+        //内容缩放最小为0.7
+        float rightScale = 0.7f+0.3f*scale;
+        ViewCompat.setPivotX(contentView,0);
+        ViewCompat.setPivotY(contentView,contentView.getMeasuredHeight()/2);
+
+        ViewCompat.setScaleX(contentView,rightScale);
+        ViewCompat.setScaleY(contentView,rightScale);
+        //菜单的缩放
+        float leftalpha = 0.7f*(1-scale)+0.7f;
+        float leftScale = 0.3f*(1-scale)+0.7f;
+        ViewCompat.setAlpha(menuView,leftalpha);
+        ViewCompat.setScaleX(menuView,leftScale);
+        ViewCompat.setScaleY(menuView,leftScale);
+        ViewCompat.setTranslationX(menuView,0.3f*l);
+    }
+
+    public void openMenu() {
+        menuState = true;
+        smoothScrollTo(0,0);
+
+    }
+
+    public void closeMenu() {
+        menuState = false;
+        smoothScrollTo(menuWidth,0);
+    }
+
     /**
      * 获取屏幕宽度
      * @return
@@ -79,4 +161,29 @@ public class XjSlidingMenu extends HorizontalScrollView {
         wm.getDefaultDisplay().getMetrics(displayMetrics);
         return displayMetrics.widthPixels;
     }
+    /**
+     * 快速滑动
+     * @param e1
+     * @param e2
+     * @param velocityX
+     * @param velocityY
+     * @return
+     */
+        private GestureDetector.SimpleOnGestureListener simpleOnGestureListener = new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (e2.getX() - e1.getX() > 100 && Math.abs(velocityX) > 200){
+                    Log.e("TAG","开发菜单");
+                    openMenu();
+                    return true;
+                }else if (e1.getX() - e2.getX() > 100 && Math.abs(velocityX) > 200){
+                    Log.e("TAG","关闭菜单");
+                    closeMenu();
+                    return true;
+                }
+                return false;
+            }
+        };
+
+
 }
