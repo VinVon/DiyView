e437d4a3fdddd237b8ea2920963d283c4595eb84
xiangwensheng <642751580@qq.com>
2017-08-13 00:02:36 +0800

    view22.view23

---
+++ app/src/main/java/com/demo/diyview/view27/Activity27.java
@@ -0,0 +1,19 @@
+package com.demo.diyview.view27;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.demo.diyview.R;
+
+/**
+ * Created by 123 on 2017/8/12.
+ */
+
+public class Activity27 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity27_layout);
+    }
+}
